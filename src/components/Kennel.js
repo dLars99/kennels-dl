3f32ff3ba5c4a561469b399cc1f2354e3d7a8ad0
David Larsen <davidplarsen@gmail.com>
2020-07-08 14:00:07 -0500

    Implement routing from Chapter 2

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,34 +1,16 @@
 import React from "react";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
+
 import "./Kennel.css";
-import AnimalCard from "./animal/AnimalCard";
-import LocationCard from "./location/LocationCard";
-import OwnerCard from "./owner/OwnerCard"
-import EmployeeCard from "./employee/EmployeeCard"
 
 const Kennel = () => {
-    return (
-        <div>
-            <div>
-                <h2>
-                    Student Kennels
-                    <br />
-                    <small>Loving care when you're not there.</small>
-                </h2>
-                <LocationCard />
-            </div>
-            <div className = "container-cards" >
-                <AnimalCard />
-                <AnimalCard />
-                <AnimalCard />
-            </div>
-            <div className = "container-cards" >
-                <OwnerCard />
-            </div>
-            <div className = "container-cards" >
-                <EmployeeCard />
-            </div>
-        </div>
-    );
+  return (
+    <>
+      <NavBar />
+      <ApplicationViews />
+    </>
+  );
 };
 
 export default Kennel;
