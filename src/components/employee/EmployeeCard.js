691d289cd560e293b586e318a682c7aa9135f0b1
David Larsen <davidplarsen@gmail.com>
2020-07-13 22:06:58 -0500

    Add employee pics

--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 const EmployeeCard = (props) => {
+
+    // Default image if no image is in the database
+    const image = !props.employee.image ? "employee-default.jpg" : props.employee.image
+
     return ( 
         <div className="card">
             <div className="card-content" >
+                <picture>
+                    <img className="card-image" src={require(`./images/${image}`)} alt="Employee" />
+                </picture>
+
                 <h3>
                     Name: <span className="card-employeename">{props.employee.name}</span>
                 </h3>
