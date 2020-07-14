5e33b389134c2bc20ed448244d40a51cfbc6106c
David Larsen <davidplarsen@gmail.com>
2020-07-14 13:53:35 -0500

    Broke everything

--- src/components/location/LocationDetail.js
+++ src/components/location/LocationDetail.js
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import LocationManager from '../../modules/LocationManager';
 import './LocationDetail.css'
+import EmployeeCard from "../employee/EmployeeCard"
 
 const LocationDetail = props => {
-    const [location, setLocation] = useState({ name: "", address: "", image: "location-default.jpg" });
+    const [location, setLocation] = useState({});
+    const [employees, setEmployees] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        //get(id) from LocationManager and hang on to the data; put it into state
-        LocationManager.get(props.locationId)
-        .then(location => {
-            setLocation({
-                name: location.name,
-                address: location.address,
-                image: location.image
-            });
+        //go here now to make call to get location with employees
+        LocationManager.getWithEmployees(props.match.params.locationId)
+        .then(APIResult => {
+            setLocation(APIResult);
+            setEmployees(APIResult.employees)
             setIsLoading(false)
         });
-    }, [props.locationId]);
+    }, []);
 
     const handleDelete = () => {
         setIsLoading(true)
@@ -28,11 +27,12 @@ const LocationDetail = props => {
         <div className="card">
         <div className="card-content">
             <picture>
-                <img className="detail-image" src={require(`./images/${location.image}`)} alt="My Dog" />
+                <img className="detail-image" src={require(`./images/${location.image}`)} alt="Location" />
             </picture>
             <h3>Location: <span style={{ color: 'darkslategrey' }}>{location.name}</span></h3>
             <p>Address: {location.address}</p>
             <button type="button" disabled={isLoading} onClick={handleDelete}>Close</button>
+            <EmployeeCard key={employees.id} employees={employees} {...props} />
         </div>
         </div>
     );
