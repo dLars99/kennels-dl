4d4cb3d24795b671a5f6a0b2ff08d2927bd79363
David Larsen <davidplarsen@gmail.com>
2020-07-15 14:55:06 -0500

    Implement location dropdown on new employee form

--- src/components/employee/EmployeeForm.js
+++ src/components/employee/EmployeeForm.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import APIManager from '../../modules/APIManager';
 import { firstLetterCase } from "../../modules/Helper"
 import './EmployeeForm.css'
 
 const EmployeeForm = props => {
-  const [employee, setEmployee] = useState({ name: "", position: "" });
-  const [isLoading, setIsLoading] = useState(false);
+  const [employee, setEmployee] = useState({ name: "", position: "", locationId: "" });
+  const [locations, setLocations] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleFieldChange = evt => {
     const stateToChange = { ...employee };
     stateToChange[evt.target.id] = firstLetterCase(evt.target.value);
     setEmployee(stateToChange);
   };
+
+  useEffect(() => {
+    APIManager.getAll("locations")
+    .then(locations => {
+      setLocations(locations)
+      setIsLoading(false)
+    })
+  }, [])
 
   /*  Local method for validation, set loadingStatus, create Employee object, invoke the APIManager post method, 
   and redirect to the full Employee list*/
@@ -21,8 +30,13 @@ const EmployeeForm = props => {
       window.alert("Please input an employee name and position");
     } else {
       setIsLoading(true);
+      const newEmployee = {
+        name: employee.name,
+        position: employee.position,
+        locationId: parseInt(employee.locationId)
+      }
       // Create the employee and redirect user to employee list
-      APIManager.post("employees", employee)
+      APIManager.post("employees", newEmployee)
         .then(() => props.history.push("/employees"));
     }
   };
@@ -48,6 +62,10 @@ const EmployeeForm = props => {
               placeholder="Position"
             />
             <label htmlFor="position">Position</label>
+            <select className="form-control" id="locationId" value={employee.locationId} onChange={handleFieldChange}>
+              {locations.map(location => <option key={location.id} value={location.id}>{location.name}</option>)}
+            </select>
+            <label htmlFor="locationId">Location</label>
           </div>
           <div className="alignRight">
             <button
