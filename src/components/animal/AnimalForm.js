520882ac8e7d2bac1dfb768b6325b15a224de44f
David Larsen <davidplarsen@gmail.com>
2020-07-15 14:37:58 -0500

    Implement employee select on new animal form

--- src/components/animal/AnimalForm.js
+++ src/components/animal/AnimalForm.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import APIManager from '../../modules/APIManager';
 import { firstLetterCase } from "../../modules/Helper"
 import './AnimalForm.css'
 
 const AnimalForm = props => {
-  const [animal, setAnimal] = useState({ name: "", breed: "" });
-  const [isLoading, setIsLoading] = useState(false);
+  const [animal, setAnimal] = useState({ name: "", breed: "", employeeId: "" });
+  const [employees, setEmployees] = useState([])
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleFieldChange = evt => {
     const stateToChange = { ...animal };
     stateToChange[evt.target.id] = firstLetterCase(evt.target.value);
     setAnimal(stateToChange);
   };
+
+  useEffect(() => {
+    APIManager.getAll("employees")
+    .then(employees => {
+      setEmployees(employees)
+      setIsLoading(false)
+    })
+  }, [])
 
   /*  Local method for validation, set loadingStatus, create animal object, invoke the APIManager post method, 
   and redirect to the full animal list*/
@@ -21,8 +30,13 @@ const AnimalForm = props => {
       window.alert("Please input an animal name and breed");
     } else {
       setIsLoading(true);
+      const newAnimal = {
+        name: animal.name,
+        breed: animal.breed,
+        employeeId: parseInt(animal.employeeId)
+      }
       // Create the animal and redirect user to animal list
-      APIManager.post("animals", animal)
+      APIManager.post("animals", newAnimal)
         .then(() => props.history.push("/animals"));
     }
   };
@@ -48,6 +62,10 @@ const AnimalForm = props => {
               placeholder="Breed"
             />
             <label htmlFor="breed">Breed</label>
+            <select className="form-control" id="employeeId" value={animal.employeeId} onChange={handleFieldChange}>
+              {employees.map(employee => <option key={employee.id} value={employee.id}>{employee.name}</option>)}
+            </select>
+            <label htmlFor="employeeId">Employee</label>
           </div>
           <div className="alignRight">
             <button
